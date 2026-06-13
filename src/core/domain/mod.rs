cd7151800e1d881d9ab92bd742bc5f28e4bd50c1
Ignacio Toledo <igna.toledot@gmail.com>
2026-06-13 01:48:18 -0400

    feat(domain): add Keystroke and KeystrokeOutcome enums

--- src/core/domain/mod.rs
+++ src/core/domain/mod.rs
@@ -2,6 +2,8 @@
 
 pub mod language;
 pub mod snippet;
+pub mod keystroke;
 
 pub use language::Language;
 pub use snippet::{Snippet, SnippetError};
+pub use keystroke::{Keystroke, KeystrokeOutcome};
